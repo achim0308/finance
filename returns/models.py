174dc01bfe98793e74c4b18fd0eb1af640afd8d8
Achim <awechsung@Maria-Stuart.local>
2017-04-28 15:15:25 +0200

    Simplified functions in TransactionManager

--- returns/models.py
+++ returns/models.py
@@ -92,6 +92,121 @@ class Account(models.Model):
     class Meta:
         ordering = ['name']
 
+class TransactionQuerySet(models.QuerySet):
+    # custom query set function for Transaction
+    def owner(self, owner_id):
+        return self.filter(owner_id=owner_id)
+    
+    def beginDate(self, beginDate):
+        return self.filter(date__gte = beginDate)
+    
+    def endDate(self, endDate):
+        return self.filter(date__lte = endDate)
+    
+    def securities(self, securities):
+        return self.filter(security_id__in = securities)
+    
+    def accounts(self, accounts):
+        return self.filter(account_id__in = accounts)
+    
+    def recent(self):
+        return self.filter(date__gte = timezone.now()+timedelta(days=-30))
+    
+    def notCashflowRelevant(self):
+    # remove transactions that are not relevant for cash flows, 
+    # i.e., interest payments or company matches on securities that accumulate interest
+        return self.filter(Q(
+            ~(
+                security__accumulate_interest = True &
+                (kind = Transaction.INTEREST | kind = Transaction.MATCH)
+            )
+        ))
+    
+    def accumulatingSecuritiesInterestAndMatch(self):
+    # show only interest and company match transactions
+    # for securities that accumulate interest
+        return self.filter(Q(
+            security__accumulate_interest = True &
+            (kind = Transaction.INTEREST | kind = Transaction.MATCH)
+        ))
+    
+    def nonMarkToMarketInAndOutflows(self):
+    # exclude only interest and company match transactions as well as 
+    # transactions for for securities that are marked to market
+        return self.exclude(security__mark_to_market = False)
+                   .exclude(kind=Transaction.INTEREST)
+                   .exclude(kind=Transaction.MATCH)
+    
+    def markToMarket(self):
+    # only select transactions of mark_to_market securities
+        return self.filter(security__mark_to_market = True)
+
+class TransactionManager2(models.Manager):
+    def get_queryset(self):
+        return TransactionQuerySet(self.model, using=self._db):
+    
+    def transactionHistory(self, beginDate = None, endDate = None,
+                           securities = None, accounts = None, owner = None):
+        transactions = self.get_queryset().order_by('date')
+        if not beginDate is None:
+            transactions = transactions.beginDate(beginDate)
+        if not endDate is None:
+            transactions = transactions.endDate(endDate)
+        if not securities is None:
+            transactions = transactions.securities(securities)
+        if not accounts is None:
+            transactions = transactions.accounts(accounts)
+        
+        return th
+    
+    def cashflow(self, beginDate = None, endDate = None,
+                       securities = None, accounts = None, owner = None)::
+    # sum daily cashflows of all transactions relevant for cashflows 
+        cashflows = self.transactionHistory(beginDate, endDate, securities, accounts)
+        cashflows = cashflows.notCashflowRelevant()
+        
+        # construct sum
+        cashflows = cashflows.values('date')
+                             .annotate(sumCashflow=Sum('cashflow'))
+        
+        return cashflows
+
+    def num(self, beginDate = None, endDate = None,
+                  securities = None, accounts = None, owner = None)::
+    # sum transacted securities
+        numSecurities = self.transactionHistory(beginDate, endDate, securities, accounts)
+        numSecurities = numSecurities.markToMarket()
+        
+        # construct sum
+        numSecurities = numSecurities.values('security_id')
+                                     .annotate(sumNumTransacted=Sum('num_transacted'))
+        
+        return numSecurities
+    
+    def curValue(self, beginDate = None, endDate = None,
+                       securities = None, accounts = None, owner = None)::
+    # sum transacted securities
+        curValues = self.transactionHistory(beginDate, endDate, securities, accounts)
+        
+        curValues1 = curValues.accumulatingSecuritiesInterestAndMatch()
+        curValues2 = curValues.nonMarkToMarketInAndOutflows()
+        
+        # sum over cashflows only
+        curValues1 = curValues1.values('security_id')
+                               .annotate(curValue=Sum(-F('cashflow')))
+        
+        # sum over cashflows minus taxes and expenses 
+        # (since these also reduce the value of security)
+        curValues2 = curValues2.values('security_id')
+                               .annotate(curValue=Sum(-(F('cashflow')
+                                                        -F('tax')
+                                                        -F('expense')))
+        
+        # combine query
+        curValues = curValues1 | curValues2
+        
+        return values
+    
 class TransactionManager(models.Manager):
     def getTransactionHistory(self, beginDate = None, endDate = None, securities = None, accounts = None, owner = None):
 
@@ -292,6 +407,7 @@ class Transaction(models.Model):
     modifiedDate = models.DateField('last modified date',
                                     db_index=True)
 
+    thobjects2 = TransactionManager2()
     thobjects = TransactionManager()
     objects = models.Manager()
 
