42a666430d9fbcc9c23b616b69608f98d8349ecf
Achim Wechsung <achim@wechsung.info>
2026-05-22 15:36:20 +0000

    Add checking for correct currency to transaction form

--- returns/forms.py
+++ returns/forms.py
@@ -13,42 +13,42 @@ class AccountForm(forms.ModelForm):
 class SecurityForm(forms.ModelForm):
 
     def clean(self):
-        if (self.cleaned_data.get('mark_to_market') == True and 
+        if (self.cleaned_data.get('mark_to_market') == True and
             self.cleaned_data.get('accumulate_interest') == True):
-            self.add_error('mark_to_market', 
+            self.add_error('mark_to_market',
                            ValidationError("Security inconsistently labeled both mark to market and accumulates interest."))
-        elif (self.cleaned_data.get('mark_to_market') == True and 
+        elif (self.cleaned_data.get('mark_to_market') == True and
               self.cleaned_data.get('url') == ''):
             self.add_error('url',
                            ValidationError("Need url for a mark to market security to get current prices."))
 
-        if (self.cleaned_data.get('kind') == Security.TAGESGELD and 
+        if (self.cleaned_data.get('kind') == Security.TAGESGELD and
             self.cleaned_data.get('accumulates_interest') == False):
             self.add_error('kind',
                            ValidationError("Inconsistent selection. This security should accumulate interest."))
 
-        if ((self.cleaned_data.get('kind') == Security.AKTIE or 
-             self.cleaned_data.get('kind') == Security.AKTIENETF or 
-#             self.cleaned_data.get('kind') == Security.BONDS or 
+        if ((self.cleaned_data.get('kind') == Security.AKTIE or
+             self.cleaned_data.get('kind') == Security.AKTIENETF or
+#             self.cleaned_data.get('kind') == Security.BONDS or
              self.cleaned_data.get('kind') == Security.BONDSETF)
              and self.cleaned_data.get('mark_to_market') == False):
             self.add_error('kind',
                            ValidationError("Inconsistent selection. This security should be marked to market."))
 
-        if (self.cleaned_data.get('accumulate_interest') == False and 
+        if (self.cleaned_data.get('accumulate_interest') == False and
             self.cleaned_data.get('calc_interest') != 0.0):
-            self.add_error('calc_interest', 
+            self.add_error('calc_interest',
                            ValidationError("Inconsistent. Fixed interest can only be used with a security that accumulates interest."))
 
-        elif (self.cleaned_data.get('accumulate_interest') == True and 
+        elif (self.cleaned_data.get('accumulate_interest') == True and
             self.cleaned_data.get('calc_interest') < 0.0):
-            self.add_error('calc_interest', 
+            self.add_error('calc_interest',
                            ValidationError("Inconsistent. Fixed interest should be positive."))
 
-        elif (self.cleaned_data.get('accumulate_interest') == True and 
-            self.cleaned_data.get('calc_interest') > 0.0 and 
+        elif (self.cleaned_data.get('accumulate_interest') == True and
+            self.cleaned_data.get('calc_interest') > 0.0 and
             self.cleaned_data.get('kind') != Security.ALTERSVORSORGE):
-            self.add_error('kind', 
+            self.add_error('kind',
                            ValidationError("Inconsistent. Fixed interest can only be used with Altersvorsorge."))
 
         return self.cleaned_data
@@ -62,7 +62,7 @@ class TransactionForm(forms.ModelForm):
         super (TransactionForm,self ).__init__(*args,**kwargs) # populates the form
         self.fields['account'].queryset = Account.objects.filter(owner=owner).active()
         self.fields['security'].queryset = Security.objects.active()
-        
+
     def clean(self):
         if (self.cleaned_data.get('kind') == Transaction.BUY):
             if self.cleaned_data.get('cashflow').amount > 0.0:
@@ -78,13 +78,13 @@ class TransactionForm(forms.ModelForm):
             if self.cleaned_data.get('num_transacted') > 0.0:
                 self.add_error('num_transacted',
                                ValidationError("Number of exchanged securities must be negative."))
-        elif (self.cleaned_data.get('kind') == Transaction.INTEREST or 
+        elif (self.cleaned_data.get('kind') == Transaction.INTEREST or
               self.cleaned_data.get('kind') == Transaction.DIVIDEND):
             if self.cleaned_data.get('cashflow').amount < 0.0:
                 self.add_error('cashflow', ValidationError("Cashflow must be positive."))
             if self.cleaned_data.get('num_transacted') != 0.0:
                 self.add_error('num_transacted', ValidationError("Number of exchanged securities must be zero."))
-        elif (self.cleaned_data.get('kind') == Transaction.INTEREST or 
+        elif (self.cleaned_data.get('kind') == Transaction.INTEREST or
               self.cleaned_data.get('kind') == Transaction.DIVIDEND or
               self.cleaned_data.get('kind') == Transaction.MATCH):
             if self.cleaned_data.get('cashflow').amount != 0.0:
@@ -100,8 +100,12 @@ class TransactionForm(forms.ModelForm):
             self.add_error('expense',
                            ValidationError("Expenses must be non-positive."))
         if (self.cleaned_data.get('tax').amount > 0.0):
-            self.add_error('tax', 
+            self.add_error('tax',
                            ValidationError("Taxes must be non-positive."))
+
+        for field in ['cashflow', 'expense', 'tax']:
+            if self.cleaned_data.get('security').currency != self.cleaned_data.get(field).currency.code:
+                self.add_error(field, ValidationError("Currency does not match"))
 
         return self.cleaned_data
 
@@ -118,7 +122,7 @@ class TransactionFormForSuperuser(TransactionForm):
         model = Transaction
         fields = ('date', 'kind', 'security', 'cashflow', 'expense', 'tax', 'account', 'num_transacted', 'owner',)
         widgets = {'date': forms.DateInput(attrs={'class':'datepicker'}),}
-        
+
 class HistValuationForm(forms.ModelForm):
 
     class Meta:
@@ -127,21 +131,21 @@ class HistValuationForm(forms.ModelForm):
         widgets = {'date': forms.DateInput(attrs={'class':'datepicker'}),}
 
 class AddInterestForm(forms.ModelForm):
-    
+
     class Meta:
         model = Transaction
         fields = ('date', 'security', 'account',)
         widgets = {'date': forms.DateInput(attrs={'class':'datepicker'}),}
 
 class AddInterestFormForSuperuser(forms.ModelForm):
-    
+
     class Meta:
         model = Transaction
         fields = ('date', 'security', 'account', 'owner',)
         widgets = {'date': forms.DateInput(attrs={'class':'datepicker'}),}
 
 class InflationForm(forms.ModelForm):
-    
+
     class Meta:
         model = Inflation
         fields = ('date', 'inflationIndex', 'country',)
